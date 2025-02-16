7274c8235b83578b5a4d5af0ba1455f542470ba2
Ravikumar Jha <jharavi153@gmail.com>
2025-02-16 19:14:55 +0530

    Functionality Implementation

--- component/08.Pagination/Page.jsx
+++ component/08.Pagination/Page.jsx
@@ -4,11 +4,12 @@ import "./Page.css"
 
 function Page() {
 
-  const [product ,setProduct] = useState([])
+  const [product ,setProduct] = useState([]);
+  const [page, setPage] = useState(1);
 
   const FetchData = async ()=>{
     try{
-      const response = await axios.get("https://picsum.photos/v2/list");
+      const response = await axios.get("https://picsum.photos/v2/list?limit=50");
       setProduct(response.data);
     }
     catch (error){
@@ -20,22 +21,36 @@ function Page() {
     FetchData();
   },[])
 
+  const handleSelectPage = (selectedPage)=>{
+    if(selectedPage >= 1 && selectedPage <= product.length && selectedPage !==page);
+    setPage(selectedPage)
+  }
+
   return (
     <div>
       { 
 
       product.length > 0 && <div className='product'> 
-        {product.map((item)=>{
+        {product.slice(page*10-10, page*10).map((item)=>{
           return <span key = {item.id} className='product_single'>
-            <img src = {item.download_url} className='img' alt = {item.id}/>
+            <img src = {item.download_url} alt = {item.id}/>
             <span>{item.author}</span>
           </span>
         })
       }
         </div>
-     
       }
-    
+
+      { product.length > 0 && <div className='pagination'>
+        <span onClick={()=>handleSelectPage(page-1)} className={page > 1 ? "" : "pagination_disable"}>◀️</span>
+        {
+        [...Array(product.length/10)].map((_, i)=>{
+          return <span key = {i} className={page == i+1 ? "pagination_selected" : ""}   onClick={()=>handleSelectPage(i+1)}>{i+1}</span>
+        })
+      }
+      <span onClick={()=>handleSelectPage(page+1)} className={page < product.length/10 ? "" : "pagination_disable" }>▶️</span>
+      </div>
+      }
     
       
     </div>
