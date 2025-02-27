c06e538c78de8497f82b636136a364b4deff9b30
Ravikumar Jha <jharavi153@gmail.com>
2025-02-27 12:19:57 +0530

    Add user

--- component/src/components/22.MultiSelect/Multi.jsx
+++ component/src/components/22.MultiSelect/Multi.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import "./style.css";
 
 function Multi() {
   const [searchItem , setSearchItem ] = useState("");
@@ -7,6 +8,10 @@ function Multi() {
   useEffect(()=>{
     const fetchData = async ()=>{
       try{
+        if(searchItem.trim() === ""){
+          setSuggestion([]);
+          return;
+        }
       const response = await fetch(`https://dummyjson.com/users/search?q=${searchItem}`);
       const data = await response.json();
       setSuggestion(data.users);
@@ -21,8 +26,26 @@ function Multi() {
 
 
    return (
-    <div>
+    <div className='search-container'>
+      <div className='search-input'>
+
+      <div>
+      <input value = {searchItem} placeholder='Search username' onChange={(e)=>setSearchItem(e.target.value)}></input>
+      <div className='suggestion-list'>
+
+        {
+          suggestion.map((item, index)=>{
+            return <li key ={index}>
+              <img src = {item.image} alt = {`${item.firstName} - ${item.lastName}`} />
+              <span>{item.firstName} {item.lastName}</span>
+            </li>
+
+          })
+        }
+      </div>
       
+    </div>
+    </div>
     </div>
   )
 }
