597a491285ef1e7c74b2cd52b587bb8ea1505e6f
Ravikumar Jha <jharavi153@gmail.com>
2025-02-28 12:13:44 +0530

    Add Component Multi

--- component/src/components/22.MultiSelect/Multi.jsx
+++ component/src/components/22.MultiSelect/Multi.jsx
@@ -1,9 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState , useRef } from 'react';
 import "./style.css";
 
 function Multi() {
   const [searchItem , setSearchItem ] = useState("");
   const [suggestion, setSuggestion ] = useState([]);
+  const [selectedUser, setSelectedUser] = useState([]);
+  const [selectedUserSet , setSelectedUserSet] = useState(new Set())
+
+  const inputRef  = useRef(null);
+
+
 
   useEffect(()=>{
     const fetchData = async ()=>{
@@ -22,32 +28,70 @@ function Multi() {
     }
 
     fetchData();
-  },[searchItem])
+  },[searchItem]);
 
+  const selectUser = (item)=>{
+    setSelectedUser([...selectedUser, item]);
+    setSelectedUserSet(new Set([...selectedUserSet , item.email]))
+    setSearchItem("");
+    setSuggestion([]);
+    inputRef.current.focus();
+
+
+  }
+
+  const handleRemoveUser = (item)=>{
+    const updateUsers = selectedUser.filter((selectedUser) =>selectedUser.id !== item.id);
+  
+    setSelectedUser(updateUsers);
+  
+    const updateEmails = new Set (selectedUserSet);
+    updateEmails.delete(item.email);
+    setSelectedUserSet(updateEmails);
+  
+  }
+
+  const handleKeyDown = (e)=>{
+    if(e.key === "Backspace" && e.target.value ==="" && selectedUser.length > 0)
+    {
+      const lastUser = selectedUser[selectedUser.length-1];
+      handleRemoveUser(lastUser);
+      setSuggestion([]);
+    }
+  }
 
    return (
     <div className='search-container'>
       <div className='search-input'>
-
-      <div>
-      <input value = {searchItem} placeholder='Search username' onChange={(e)=>setSearchItem(e.target.value)}></input>
-      <div className='suggestion-list'>
-
         {
-          suggestion.map((item, index)=>{
-            return <li key ={index}>
-              <img src = {item.image} alt = {`${item.firstName} - ${item.lastName}`} />
-              <span>{item.firstName} {item.lastName}</span>
-            </li>
-
+          selectedUser.map((item)=>{
+            return <span key = {item.id} className='user-pill'>
+              <span><img src = {item.image}></img></span>
+              <span onClick={()=>handleRemoveUser(item)}>{item.firstName} &times;</span>
+              </span>
           })
         }
+
+      <div>
+      <input value = {searchItem} placeholder='Search username' onChange={(e)=>setSearchItem(e.target.value)} ref = {inputRef}
+      onKeyDown={handleKeyDown}
+      ></input>
+      {suggestion.length > 0 && (
+            <ul className="suggestion-list">
+              {suggestion.map((item) =>
+                !selectedUserSet.has(item.email) ? (
+                  <li key={item.email} onClick={() => selectUser(item)}>
+                    <img src={item.image} alt={`${item.firstName} ${item.lastName}`} />
+                    <span>{item.firstName} {item.lastName}</span>
+                  </li>
+                ) : null
+              )}
+            </ul>
+          )}
+        </div>
       </div>
-      
     </div>
-    </div>
-    </div>
-  )
+  );
 }
 
-export default Multi
+export default Multi;
